68bae8e8f8836b754bd27031dd458f6fe3bdd827
Xavierize <xkajack@gmail.com>
2022-11-07 19:49:49 -0500

    Encountered bug!

---
+++ RPS.js
@@ -0,0 +1,9 @@
+const getComputerChoice = () => {
+    const arrOfChoices = ["Rock",
+     "Paper",
+     "Scissors"]
+    const randomNum = Math.floor(Math.random() * 3)
+    console.log(["Rock", "Paper", "Scissors"])
+}
+
+getComputerChoice();
